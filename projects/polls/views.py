4aeeba88ef1f91f6ab0d99c04998f9f5ab37df94
Sid Hayoun Lee <sidhlee@gmail.com>
2021-11-14 17:19:17 -0500

    Add logic to vote view

--- projects/polls/views.py
+++ projects/polls/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, Http404
+from django.http.response import HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -21,4 +23,27 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # submitted data is available under request.POST by the input name
+        # this will raise KeyError if it can't find "choice" in request.POST
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You didn't select a choice."},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # Must return HttpResponseRedirect after dealing with POST data to prevent
+        # the user hitting the Back button and submitting the POST data twice.
+        return HttpResponseRedirect(
+            # just like we use url template tag to access path by its name and argument,
+            # we can use reverse inside view function to access routes defined in url.py
+            reverse("polls:results", args=(question.id,))
+        )
+
     return HttpResponse(f"You're voting on question {question_id}")
