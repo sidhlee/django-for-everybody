abc1047b4ae804d6f50ec72cd432b653bf9f3347
Sid Hayoun Lee <sidhlee@gmail.com>
2021-11-14 11:57:05 -0500

    Replace hardcoded URLs in templates with path name

--- projects/polls/urls.py
+++ projects/polls/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     # <> captures part of url and sends it as a keyword argument to the view function
     # ex: /polls/5/
+    # we can acesss 'detail' url from the template using {% url %} template tag and provide argument
+    # to be inserted as question_id
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
