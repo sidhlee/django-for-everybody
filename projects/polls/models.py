b37d7da22fb0fe2628572656e7e8e668cfee0836
Sid Hayoun Lee <sidhlee@gmail.com>
2021-11-13 20:49:44 -0500

    Add models

--- projects/polls/models.py
+++ projects/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    # max_length is required for CharField
+    question_text = models.CharField(max_length=200)
+    # optional first positional arg for human-readable field name
+    # Also works as documentation
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
