a21bf9bfe9ea07191f33ba445782b2f2e1adb766
Sid Hayoun Lee <sidhlee@gmail.com>
2021-11-13 22:32:44 -0500

    Update root view + create template

--- projects/polls/urls.py
+++ projects/polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # eg: /polls/
     path("", views.index, name="index"),
+    # <> captures part of url and sends it as a keyword argument to the view function
     # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
